b7c36298a92b7ea3f63c09865cf1cfad345c3560
Namaskaro <45168111+Namaskaro@users.noreply.github.com>
2021-09-28 14:04:34 +0300

    prerender start

--- build.prerender.js
+++ build.prerender.js
@@ -1,8 +1,26 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const proxy = require('./proxy.server');
 
 module.exports = (api, options) => {
+  let staticDir = path.join(__dirname, options.outputDir);
+  let outputDir = path.join(__dirname, 'prerender');
+  let routes = ['/'];
   api.registerCommand('build:prerender', async args => {
+    api.chainWebpack(config => {
+      config.plugin('prerender').use(PrerenderSPAPlugin, [
+        {
+          staticDir,
+          outputDir,
+          routes,
+          renderer: new PrerenderSPAPlugin.PuppeteerRenderer({
+            renderAfterDocumentEvetn: 'renderer-ready',
+          }),
+          server: { proxy },
+        },
+      ]);
+    });
+
     await api.service.run('build');
   });
 };
---
+++ proxy.server.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '/api/v1': {
+    target: 'http://localhost:4000',
+    secure: false,
+    changeOrigin: true,
+  },
+};
